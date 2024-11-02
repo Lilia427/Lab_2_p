b38e611966cc480648ec0a0121821ca6c9661dd8
Liliia Zuziak <liliazuziak724@gmail.com>
2024-11-02 12:54:42 +0200

    Initial commit

---
+++ calculator/__init__.py
@@ -0,0 +1,7 @@
+from .calculator import Calculator
+from .operations import Operations
+from .memory import Memory
+from .history import History
+from .settings import Settings
+
+__all__ = ["Calculator", "Operations", "Memory", "History", "Settings"]
---
+++ main.py
@@ -0,0 +1,5 @@
+from calculator.calculator import Calculator
+
+if __name__ == "__main__":
+    calculator = Calculator(decimal_places=2)
+    calculator.run()
